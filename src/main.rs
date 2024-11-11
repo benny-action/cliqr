86394668f7427012cc79fa5d66044b4698e1eb72
benny <contact.ben.perkins@gmail.com>
2024-11-11 20:55:46 +0000

    added mode indicator and beginnings of function to define qr size

--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,7 @@ impl QRBody {
         print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
     }
 }
-
-//TODO: add: mode encoding, error correction level,
+//TODO: add: error correction level,
 //qr code version used, character count indicator,
 //padding
 
@@ -41,10 +40,14 @@ fn main() {
     println!("CLIQR: text to QR in command line");
     let new_input = get_string_input("Enter text to become QR: ");
     qr_translate_app.add_text(&new_input);
-    let binary_info = to_binary(&new_input);
+
+    let ip_length = new_input.len();
+    println!("{}", ip_length); //output to version function to get info for sizing
+
+    //QR code printer
+    let binary_info = "0100".to_owned() + &to_binary(&new_input); //has byte-mode indicator prepend
     let qr_info = ansi_translate(&binary_info);
     //TODO: Refactor to reduce mem footprint
-    println!("{}", new_input);
     println!("{}", binary_info);
     println!("{}", qr_info);
 }
@@ -78,4 +81,17 @@ fn ansi_translate(input: &str) -> String {
             _ => c,
         })
         .collect()
+}
+fn error_correction() {
+    //function to inplement M level error correction - to recover up to 15% of the data, just use a
+    //reed solomon crate
+}
+fn smallest_version(string_length: i32) -> (i32, i32) {
+    //to determine the smallest possible version of the qr, min level 25x, max level ver40 177x
+    //TODO: add match casees for other versions, only have ver 1 here, increment by 4
+    let len = string_length;
+    match len {
+        1..=15 => (25, 25),
+        _ => (0, 0),
+    }
 }
